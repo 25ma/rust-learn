db5311731300e1a2d2bb6763ab3a5531bd0b8fae
25ma <25mateam@gmail.com>
2021-09-01 23:57:13 +0800

    ./.gitignore

---
+++ l01_stack_heap/target/debug/L01_stack_heap.d
@@ -0,0 +1,1 @@
+E:\rust\rust-learn\l01_stack_heap\target\debug\L01_stack_heap.exe: E:\rust\rust-learn\l01_stack_heap\src\main.rs
---
+++ l01_stack_heap/target/debug/deps/L01_stack_heap.d
@@ -0,0 +1,5 @@
+E:\rust\rust-learn\L01_stack_heap\target\debug\deps\L01_stack_heap.exe: src\main.rs
+
+E:\rust\rust-learn\L01_stack_heap\target\debug\deps\L01_stack_heap.d: src\main.rs
+
+src\main.rs:
