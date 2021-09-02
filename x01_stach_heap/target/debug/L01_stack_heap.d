3e2d90c1feebbf16335621af419605bc4e508e6f
25ma <25mateam@gmail.com>
2021-09-02 13:46:21 +0800

    修改忽略问题件

--- x01_stach_heap/target/debug/L01_stack_heap.d
+++
@@ -1,1 +0,0 @@
-D:\workspace\learn\rust\rust-learn\l01_stack_heap\target\debug\L01_stack_heap.exe: D:\workspace\learn\rust\rust-learn\l01_stack_heap\src\main.rs
--- x01_stach_heap/target/debug/deps/L01_stack_heap.d
+++
@@ -1,5 +0,0 @@
-D:\workspace\learn\rust\rust-learn\l01_stack_heap\target\debug\deps\L01_stack_heap.exe: src\main.rs
-
-D:\workspace\learn\rust\rust-learn\l01_stack_heap\target\debug\deps\L01_stack_heap.d: src\main.rs
-
-src\main.rs:
